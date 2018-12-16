67c959eef5876f323013547b4f242bfb6369a46f
Alon Alush <alon.a@syte-vc.com>
2018-12-16 03:10:30 +0200

    add abour css grid

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -3,7 +3,11 @@ import './About.scss';
 
 const About = (props) => (
   <div className="About">
-    Fotter
+    {/* <div className="Box">ff</div> */}
+    <div className="Box1">ff</div>
+    {/* <div className="Box2">ff</div> */}
+    <div className="Box3">ff</div>
+    <div className="Box4">ff</div>
   </div>
 );
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Layout.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../../components/Header/Header';
@@ -6,7 +6,7 @@ import Footer from '../../components/Footer/Footer';
 
 const Layout = (props) => {
   const layoutClasses = props.togglePosition ? 'Layout Fixed' : 'Layout';
-console.log('props.togglePosition',props.togglePosition)
+
   return (
     <div className={layoutClasses} >
       <Header />
