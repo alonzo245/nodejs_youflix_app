3dd85d0164a9194009e3e25e742e54c97ab29d91
Alon Alush <alon.a@syte-vc.com>
2018-12-17 23:31:33 +0200

    theme css changes

--- src/components/UI/Toggle/Toggle.js
+++ src/components/UI/Toggle/Toggle.js
@@ -4,19 +4,12 @@ import './Toggle.scss';
 const Toggle = (props) => {
 
   return (
-    // <div className="ToggleCheckbox" onChange={props.toggleTheme}>
-    //   <label className="switch">
-    //     <input type="checkbox" />
-    //     <span className="slider round"></span>
-    //   </label>
-    // </div>
-
     <div className="ToggleCheckbox" onChange={props.toggleTheme}>
       <div class="can-toggle demo-rebrand-2">
         <input id="e" type="checkbox" />
         <label for="e">
-          <div class="can-toggle__label-text">Dark Theme </div>
-          <div class="can-toggle__switch" data-checked="On" data-unchecked="Off"></div>
+          <div class="can-toggle__label-text">Switch Theme </div>
+          <div class="can-toggle__switch" data-checked="Off" data-unchecked="On"></div>
         </label>
       </div>
     </div>
