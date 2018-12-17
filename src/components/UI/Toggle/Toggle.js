aba68dba72f64a2c6f6c3d87d9d2a5d25f17622f
Alon Alush <alon.a@syte-vc.com>
2018-12-17 17:31:08 +0200

    more css mobile adjusment

--- src/components/UI/Toggle/Toggle.js
+++ src/components/UI/Toggle/Toggle.js
@@ -4,11 +4,21 @@ import './Toggle.scss';
 const Toggle = (props) => {
 
   return (
+    // <div className="ToggleCheckbox" onChange={props.toggleTheme}>
+    //   <label className="switch">
+    //     <input type="checkbox" />
+    //     <span className="slider round"></span>
+    //   </label>
+    // </div>
+
     <div className="ToggleCheckbox" onChange={props.toggleTheme}>
-      <label className="switch">
-        <input type="checkbox" />
-        <span className="slider round"></span>
-      </label>
+      <div class="can-toggle demo-rebrand-2">
+        <input id="e" type="checkbox" />
+        <label for="e">
+          <div class="can-toggle__label-text">Dark Theme </div>
+          <div class="can-toggle__switch" data-checked="On" data-unchecked="Off"></div>
+        </label>
+      </div>
     </div>
   )
 };
