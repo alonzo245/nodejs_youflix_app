bab11c34769fdc4f46838555ef182403e51c599c
Alon Alush <alon.a@syte-vc.com>
2018-12-29 21:35:29 +0200

    added social mockup

--- src/containers/Social/Social.js
+++ src/containers/Social/Social.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './Social.scss';
+import Logo from '../../../src/assets/images/youflix.png';
 
 const Social = (props) => (
   <div className="Social">
-    <div className="Box">
-      <p>Social</p>
-      <span>Project Source code =></span>
+    <div className="w3-row-padding">
+      <div className="w3-col m12">
+        <div className="w3-card-2 w3-round w3-white">
+          <div className="w3-container w3-padding">
+            <h6 className="w3-opacity">Social Media template by </h6>
+            <p contenteditable="true" className="w3-border w3-padding">What Your Feeling...?</p>
+            <button type="button" className="w3-button w3-theme"><i className="fa fa-pencil"></i> &nbsp;Post</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div className="w3-container w3-card-2 w3-white w3-round w3-margin"><br />
+      <img src={Logo} alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{ "width": "60px" }} />
+      <span className="w3-right w3-opacity">1 min</span>
+      <h4>John Doe</h4><br />
+      <hr className="w3-clear" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div className="w3-row-padding" style={{ "margin": "0 -16px" }}>
+        <div className="w3-half">
+          <img src={Logo}style={{ "width": "100%" }} alt="Northern Lights" className="w3-margin-bottom" />
+        </div>
+        <div className="w3-half">
+          <img src={Logo} style={{ "width": "100%" }} alt="Nature" className="w3-margin-bottom" />
+        </div>
+      </div>
+      <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up"></i> Like</button>
+      <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment"></i>Comment</button>
     </div>
   </div>
 );
